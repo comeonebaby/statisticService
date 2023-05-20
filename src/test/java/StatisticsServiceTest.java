51fafa4942111f8cbba52ac75d0149b74155c2c8
comeonebaby <124260046+comeonebaby@users.noreply.github.com>
2023-05-20 15:24:41 +0200

    Update StatisticsServiceTest.java

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -31,7 +31,7 @@ void findMaxFromSingleIncome() {
 void findMaxFromNegativeIncomes() {
     StatisticsService service = new StatisticsService();
     long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
-    long expected = 0;
+    long expected = -3;
     long actual = service.findMax(incomesInBillions);
     Assertions.assertEquals(expected, actual);
 }
