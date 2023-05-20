b45e01ab4824561acea31e3df445df0a9e9c4f3b
comeonebaby <124260046+comeonebaby@users.noreply.github.com>
2023-05-20 15:12:43 +0200

    Update StatisticsServiceTest.java

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -1,32 +1,24 @@
 package ru.netology.statistic;
-
 import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
-
 public class StatisticsServiceTest {
-
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
         Assertions.assertEquals(expected, actual);
     }
+    
     @Test
     void findCycle() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7};
         long expected = 7;
-
         long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);}
+        Assertions.assertEquals(expected, actual);
+}  
     @Test
 void findMaxFromSingleIncome() {
     StatisticsService service = new StatisticsService();
@@ -35,5 +27,12 @@ void findMaxFromSingleIncome() {
     long actual = service.findMax(incomesInBillions);
     Assertions.assertEquals(expected, actual);
 }
+@Test
+void findMaxFromNegativeIncomes() {
+    StatisticsService service = new StatisticsService();
+    long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+    long expected = 0;
+    long actual = service.findMax(incomesInBillions);
+    Assertions.assertEquals(expected, actual);
 }
-
+}
